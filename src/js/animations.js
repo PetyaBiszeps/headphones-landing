1216681bce0ac76919e3e7d01b9521cd42a27be7
Yaroslav Petrov <PersonalYP00@gmail.com>
2024-09-11 16:14:33 +0300

    commit changes

--- src/js/animations.js
+++ src/js/animations.js
@@ -3,13 +3,13 @@ export function animationPack() {
         // reset: true,
         distance: '60px',
         duration: 800,
-        delay: 400,
+        delay: 100,
         // origin: top / left / right / bottom
     });
 
-    sr.reveal('.home__header, .section__title', {delay: 400});
-    sr.reveal('.home__footer', {delay: 600});
-    sr.reveal('.home__img', {delay: 800, origin: 'top'});
+    sr.reveal('.home__header, .section__title', {delay: 0});
+    sr.reveal('.home__footer', {delay: 100});
+    sr.reveal('.home__img', {delay: 200, origin: 'top'});
 
     sr.reveal('.sponsor__img, .product__card', {origin: 'top', interval: 100});
     sr.reveal('.specs__data, .discount__animate', {origin: 'left', interval: 100});
